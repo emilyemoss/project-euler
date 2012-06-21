757851d1d6a64dd17e0cc39634318f067d09df09
Emily Moss <emilyemoss@gmail.com>
2012-06-21 01:00:01 -0400

    finalize problem 1

--- problems/1.php
+++ problems/1.php
@@ -1,13 +1,36 @@
 <?php
 
-$counter = 0;
+$sum = 0;
 
 for ($i=1; $i < 1000; $i++) {
 	if ($i % 3 == 0 || $i % 5 == 0) {
-		$counter += $i;
+		$sum += $i;
 	}	
 }
 
-echo "\n TOTAL = " . $counter;
+echo "TOTAL = " . $sum . "<br>";
+
+
+
+// alternative (more efficient) solution: 
+// calculate the sum of numbers < 1000, divisible by 3
+// + the sum of numbers < 1000, divisible by 5
+// - the sum of numbers < 1000, divisible by 15. 
+// 
+
+$target = 999;
+$sum = 0;
+
+function counted($a) 
+{
+	global $target; // must state to use global $target within function
+	$p = floor($target/$a); // floor to round down after division
+	return ($a*($p*($p+1))/2);
+}
+
+$sum = counted(3) + counted(5) - counted(15);
+echo "TOTAL = " . $sum . "<br>";
+
+
 
 ?>
